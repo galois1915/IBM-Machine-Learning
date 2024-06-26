f4497ae6afbd952f4265a55d5b42f69eee22e8dc
galois2023 <angel20170238b@gmail.com>
2024-06-26 12:54:50 -0500

    classification knn

---
+++ Supervised-Classification/application/app.py
@@ -0,0 +1,3 @@
+## Human Activity Recognition with Smartphones
+## nutrition information about food items for people with diabetes
+## telecom industry, which includes customer data such as long-distance usage, data usage, monthly revenue, types of offerings, and other services purchased by customers.
