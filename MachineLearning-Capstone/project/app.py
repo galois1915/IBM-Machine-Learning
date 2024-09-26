451dceda4787446e39751525343bbdca02c61ac5
angel <angel20170238b@gmail.com>
2024-09-26 17:33:06 -0500

    commit

--- MachineLearning-Capstone/project/app.py
+++ MachineLearning-Capstone/project/app.py
@@ -19,11 +19,57 @@ The main grading criteria will be:
 import streamlit as st
 import numpy as np
 
-tab1, tab2 = st.tabs(["Introduction","EDA","K Means", "NMF", "NN"])
-data = np.random.randn(10, 1)
+tab1, tab2, tab3, tab4, tab5 = st.tabs(["Introduction",
+                                        "EDA",
+                                        "User profile and course genres", 
+                                        "Course similarity",
+                                        "Clustering"])
 
+
+
+## INTRODUCTION
 tab1.subheader("A tab with a chart")
-tab1.line_chart(data)
+tab1.markdown('''
+              ##### Overview project
+              Overview
+              ''')
 
-tab2.subheader("A tab with the data")
-tab2.write(data)
+
+
+## EDA
+tab2.subheader("Exploratory data Analysis")
+tab2.markdown('''
+              ##### Showing the analysis
+              BoW, etc
+              ''')
+
+## USER PROFILE AND COURSE GENRES
+tab3.subheader('USER PROFILE AND COURSE GENRES')
+tab3.markdown('''
+              ##### Recommed the top best courses to users
+              We have the **profile** of the users and the courses which are the users **enrolles**. 
+              We will recommend the couress no seen based on his profile. The processed consist in the following steps:
+              - Load the file with the info of enrollens, generate the dataframe with the recommendations for users.
+              - Show the dataframe and add button if you want save the dataframe. Apply filter if you want.
+              ''')
+
+## COURSE SIMILARITY
+tab4.subheader('Course similarity')
+tab4.markdown('''
+              ##### Recommed courses bases to especific course
+              We have the description of each course, in this case will recommend courses bases in the
+              enrollement of user. We upload the file with the enrollemnet and the output will be 
+              recommend courses based on it.
+              ''')
+
+## CLUSTERING
+tab5.subheader('Clustering')
+tab5.markdown('''
+              ##### Cluster users based in profile
+              Using the profile of the users, we will cluster them using K-Means and PCA. Generate a dataframe 
+              that have the labels for each user. With the new groups, we will recommend the top courses in the 
+              group which the user belong. You can show the recommended course using a fataframe or can apply filters.
+              In this case you also need to upload the file with the enrollements.
+
+              > Keep in ming that some users may not be in  the upload file.
+              ''')
